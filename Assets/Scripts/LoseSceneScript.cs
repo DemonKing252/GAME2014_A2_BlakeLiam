ad225c9147c0bb5227c2cd258ec7ee281cfca1c3
DemonKing252 <46793138+DemonKing252@users.noreply.github.com>
2020-11-14 20:55:16 -0500

    Added Lose Scene and Win Scene.

---
+++ Assets/Scripts/LoseSceneScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseSceneScript : MonoBehaviour
+{
+
+    [SerializeField]
+    AudioSource theme, btnClick;
+
+    //
+    // Start is called before the first frame update
+    void Start()
+    {
+        theme.Play();
+        if (Utilities.scenesChanged == 0)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void PlayAgain()
+    {
+        btnClick.Play();
+        Utilities.scenesChanged++;
+        SceneManager.LoadScene(3);
+    }
+    public void BackToMenu()
+    {
+        btnClick.Play();
+        Utilities.scenesChanged++;
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/OverworldScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OverworldScript : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource btnClick;
+    /* NOTE: Temporary variables to prove that each scene works */
+
+    public void LoadLoseScene()
+    {
+        btnClick.Play();
+        Utilities.scenesChanged++;
+        SceneManager.LoadScene(4);
+    }
+    public void LoadWinScene()
+    {
+        btnClick.Play();
+        Utilities.scenesChanged++;
+        SceneManager.LoadScene(5);
+    }
+}
